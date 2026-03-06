ea6659c7ee71dccb6d806e1a1c31ec98458f1879
Mesgun <Mesgun@MESGUN->
2026-03-06 06:49:12 +0100

    lägg till ordersida och endpoint för visa order info

---
+++ BankBlazorAPI/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BankBlazorAPI.Models;
+
+namespace BankBlazorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AdventureWorksLt2022Context _context;
+
+        public OrdersController(AdventureWorksLt2022Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOrder(int id)
+        {
+            var order = await _context.SalesOrderHeaders
+                .Where(o => o.SalesOrderId == id)
+                .Select(o => new
+                {
+                    o.SalesOrderId,
+                    o.OrderDate,
+                    o.TotalDue
+                })
+                .FirstOrDefaultAsync();
+
+            if (order == null)
+                return NotFound();
+
+            return Ok(order);
+        }
+    }
+}
